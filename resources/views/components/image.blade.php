b73c70b568b0006be9eaa2e3fa5220457dbc4938
mahroof-cp <mahroof9726@gmail.com>
2024-10-09 00:14:30 +0530

    image issue cleared

--- resources/views/components/image.blade.php
+++ resources/views/components/image.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group">
     <label for="current_image">Current Image:</label><br>
     @if($image)
-        <img src="{{ asset('storage/images/' . $image) }}" alt="Current Image" style="max-width: 200px;">
+        <img src="{{ asset('storage/image/' . $image) }}" alt="Current Image" style="max-width: 200px;">
     @else
         <p>No current image found.</p>
     @endif
--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group">
     <label for="current_image">Current Logo:</label><br>
     @if($logo)
-        <img src="{{ asset('storage/images/' . $logo) }}" alt="Current Logo" style="max-width: 200px;">
+        <img src="{{ asset('storage/logo/' . $logo) }}" alt="Current Logo" style="max-width: 200px;">
     @else
         <p>No current logo found.</p>
     @endif
